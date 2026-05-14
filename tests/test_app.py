2d9b25a9c257ee1d6787a36762c51191102d8819
psnirmala <periasamy.subramanian@gmail.com>
2026-05-14 16:06:01 +0530

    second commit

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,13 @@
 import unittest
-from .. import app
+import os
+import sys
 
 class BasicTests(unittest.TestCase):
+   def test_main_page(self): 
+    current_dir=os.path.dirname(os.path.abspath(__file__))
+    parent_dir=os.path.dirname(current_dir)
+    sys.path.append(parent_dir)
+    import app
     app1=app.app
     tester=app1.test_client(self)
     response=tester.get("/",content_type='html/text')
