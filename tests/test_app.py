9862bd62b4c92cbe23a8ce64d241d63cc400aae0
psnirmala <periasamy.subramanian@gmail.com>
2026-05-14 15:58:51 +0530

    first commit

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,9 @@
 import unittest
-from app import app
+from .. import app
 
 class BasicTests(unittest.TestCase):
-    tester=app.test_client(self)
+    app1=app.app
+    tester=app1.test_client(self)
     response=tester.get("/",content_type='html/text')
     self.assertEqual(response.status_code,200)
     self.assertEqual(response.data,b"HEllo, Jenkins CI/CD from Flask App on Windows!")
